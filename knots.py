ab7f04ae4dfc05b79b2112de5b97d9ea6e1ab49d
anvitha305 <44482134+anvitha305@users.noreply.github.com>
2022-09-23 02:56:23 -0400

    Update knots.py
    Draws the unknot now !!!

--- knots.py
+++ knots.py
@@ -13,18 +13,21 @@ class App(tk.Frame):
         super().__init__(master)
         self.pack()
 
-        self.dowker = tk.Entry()
-        
+        self.dowker = tk.Entry(width=57)
+        self.canvas = tk.Canvas(width=400, height=300, background='chartreuse')
+        self.canvas.pack(side="bottom")
 
         # default Dowker encoding is the trefoil [4, 6, 2]
         self.dft = tk.StringVar()
         self.dft.set("[4, 6, 2]")
         self.dowker["textvariable"] = self.dft
-        self.dowker.pack()
+        self.dowker.pack(side="left")
         
         # to submit Dowker encoding for parsing and . Drawing.
         self.smt = tk.Button(text="Submit", command=self.parseDowker)
-        self.smt.pack()
+        self.smt.pack(side="right")
+        
+       
         # Define a binding for when the user hits return because I'm
         # stupid and hit the return key instead of hitting buttons
         
@@ -33,12 +36,17 @@ class App(tk.Frame):
         
 
     def parseDowker(self, event=None):
+        # Have to clear the canvas prior to 
+        self.canvas.delete("all")
         """ 
         Splits the input into a list of integers a la typical
         Dowker encoding notation
         """
-        enc = list(int(a.split()[0]) for a in 
-                   re.split(",|\[|\]",self.dowker.get()) if a!="")
+        try:
+            enc = list(int(a.split()[0]) for a in 
+                       re.split(",|\[|\]",self.dowker.get()) if a!="")
+        except ValueError:
+            raise Exception("Dowker codes are comprised of a list of numbers!!")
         
         # Dowker encodings only have unique crossing labels
         if list(dict.fromkeys(enc)) != list(abs(e) for e in enc):
@@ -53,7 +61,13 @@ class App(tk.Frame):
                 raise Exception("Encoding values out of range of " +
                                 "number of crossings in knot")
         
+        if not mx: 
+        # draws the unknot if nothing's in the Dowker encoding
+             self.canvas.create_oval(125, 75, 275, 225, width=7)
+        
+        print((self.canvas.winfo_width(), self.canvas.winfo_height()))
         print(enc)
+        
         
 
 root = tk.Tk()
